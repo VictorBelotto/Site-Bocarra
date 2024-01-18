c63b8aa9678db17551eaa6051cacaf1f3a8bb5c0
VictorBelotto <victortucunduva.vb@outlook.com>
2024-01-18 15:12:38 -0300

    proporção 2 /3 mobile slide

--- src/functions/exportImagesCircos/imagensCircoFantastico.js
+++ src/functions/exportImagesCircos/imagensCircoFantastico.js
@@ -1,10 +1,11 @@
 import img1 from '../../assets/images/circo_fantastico/1.jpg'
 import img2 from '../../assets/images/circo_fantastico/2.jpg'
 import img3 from '../../assets/images/circo_fantastico/3.jpg'
+import img4 from '../../assets/images/circo_fantastico/4.jpg'
 
 
 const imagens = {
-  circoFantastico: [img1, img2, img3]
+  circoFantastico: [img1, img2, img4, img3 ]
 }
 
 export default imagens
--- src/functions/exportImagesCircos/imagensCircoRamito.js
+++ src/functions/exportImagesCircos/imagensCircoRamito.js
@@ -1,10 +1,11 @@
 import img1 from '../../assets/images/circo_ramito/1.jpg' 
 import img2 from '../../assets/images/circo_ramito/2.jpg' 
 import img3 from '../../assets/images/circo_ramito/3.jpg' 
+import img4 from '../../assets/images/circo_ramito/4.jpg' 
 
 
 const imagens = {
-  circoRamito: [img1, img2, img3]
+  circoRamito: [img1, img2, img4,  img3]
 }
 
 export default imagens
