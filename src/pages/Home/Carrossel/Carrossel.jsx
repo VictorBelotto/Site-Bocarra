b16f761f76ff5c0932976eb2ebe2afcca431c8cb
VictorBelotto <victortucunduva.vb@outlook.com>
2024-01-30 15:33:06 -0300

    Slide autoPlay home

--- src/pages/Home/Carrossel/Carrossel.jsx
+++ src/pages/Home/Carrossel/Carrossel.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styles from './carrossel.module.css';
 import exportImagesCarrossel from './exportImagesCarrossel/';
-import { GoArrowRight } from "react-icons/go";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 const Carrossel = () => {
-  const imagens = exportImagesCarrossel;
-  const [index, setIndex] = React.useState(0);
-  setTimeout(
-    () => {
-      if(index < (imagens.length - 1)){
-        setIndex(index + 1)
-      } else {
-        setIndex(0)
-      }
-    }, 4000
-  );
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty('--progress', 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  };;
 
   return (
     <div className={styles.container_carrossel}>
@@ -24,12 +23,36 @@ const Carrossel = () => {
         <h1>Lonas de Circo</h1>
         <a href='/projetos'>Acesse a galeria</a>
       </div>
-      <div className={styles.slide}>
-          <img
-            src={imagens[index]}
-            className={styles.imagem}
-          />
-      </div>
+
+        <Swiper 
+        className={styles.swiper}
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          onAutoplayTimeLeft={onAutoplayTimeLeft}
+          loop={true}
+        >
+        {exportImagesCarrossel.map((imagem) =>(
+          <SwiperSlide key={imagem} className={styles.slide}>
+            <img  src={imagem} className={styles.imagem}/>
+          </SwiperSlide>
+        ))}
+         <div className={styles.autoplay_progress} slot="container-end">
+            <svg viewBox="0 0 48 48" ref={progressCircle}>
+              <circle cx="24" cy="24" r="20"></circle>
+            </svg>
+            <span ref={progressContent}></span>
+          </div>
+      </Swiper>          
+    
     </div>
   );
 };
